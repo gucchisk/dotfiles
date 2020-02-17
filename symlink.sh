321ea885478436fa0aa47f5896a1473eb53dbb5b
gucchisk <gucchi_sk@yahoo.co.jp>
2020-02-17 23:54:57 +0900

    Add mkcd.fish

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ln -s $(cd $(dirname $0); pwd)/.emacs ~/.emacs
+ls $(cd $(dirname $0); pwd)/.config/fish/functions/ | xargs -I{} ln -s $(cd $(dirname $0); pwd)/.config/fish/functions/{} ~/.config/fish/functions/{}
