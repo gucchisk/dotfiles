4f8c8631b221128d51726ab0807e7ff342dc9424
gucchisk <gucchi_sk@yahoo.co.jp>
2020-03-16 19:56:29 +0900

    Add gitconfig

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,4 @@
 ln -s $(cd $(dirname $0); pwd)/.emacs ~/.emacs
 ls $(cd $(dirname $0); pwd)/.config/fish/functions/ | xargs -I{} ln -s $(cd $(dirname $0); pwd)/.config/fish/functions/{} ~/.config/fish/functions/{}
 ln -s $(cd $(dirname $0); pwd)/.emacs.d/elisp ~/.emacs.d/elisp
+ln -s $(cd $(dirname $0); pwd)/.gitconfig ~/.gitconfig
