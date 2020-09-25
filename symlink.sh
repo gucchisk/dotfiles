3f75262c857f02ebf5436e0f05413e0ecdfda31a
gucchisk <gucchi_sk@yahoo.co.jp>
2020-09-25 23:50:03 +0900

    Refactor symlink script

--- symlink.sh
+++ symlink.sh
@@ -2,9 +2,14 @@
 
 homedir=$(cd $(dirname $0); pwd)
 dotfilelist=(.emacs .emacs.d/elisp .emacs.d/conf .gitconfig .emacs.d/custom.el)
+dotfiledirlist=(.config/fish/functions .config/fish/conf.d)
 
-ls $(cd $(dirname $0); pwd)/.config/fish/functions/ | xargs -I{} ln -s $(cd $(dirname $0); pwd)/.config/fish/functions/{} ~/.config/fish/functions/{}
-ls $(cd $(dirname $0); pwd)/.config/fish/conf.d/ | xargs -I{} ln -s $(cd $(dirname $0); pwd)/.config/fish/conf.d/{} ~/.config/fish/conf.d/{}
+for dir in ${dotfiledirlist[@]}; do
+    files=$(ls $dir)
+    for file in $files; do
+	dotfilelist+=( $dir/$file )
+    done
+done
 
 for file in ${dotfilelist[@]}; do
     src=$homedir/$file
