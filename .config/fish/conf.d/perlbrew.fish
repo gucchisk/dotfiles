53746b8b36d3b17e1916b353fa668af9a9d674dd
gucchisk <gucchi_sk@yahoo.co.jp>
2021-03-13 00:36:58 +0900

    Add perlbrew & rakubrew

---
+++ .config/fish/conf.d/perlbrew.fish
@@ -0,0 +1,3 @@
+if set -q use_perlbrew; and $use_perlbrew
+    source $HOME/perl5/perlbrew/etc/perlbrew.fish
+end
---
+++ .config/fish/conf.d/rakubrew.fish
@@ -0,0 +1,3 @@
+if set -q use_rakubrew; and $use_rakubrew
+    /Users/gucchi/.rakubrew/bin/rakubrew init Fish | source
+end
