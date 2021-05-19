11f7e3a07e7cfe67f402e12867c1b4d1ec97403f
gucchisk <gucchi_sk@yahoo.co.jp>
2021-05-20 01:11:09 +0900

    Add gcloudsdk & homebrew config

---
+++ .config/fish/conf.d/gcloudsdk.fish
@@ -0,0 +1,3 @@
+if set -q use_gcloud; and $use_gcloud
+    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+end
---
+++ .config/fish/conf.d/homebrew.fish
@@ -0,0 +1,3 @@
+if set -q use_brew_sphinx; and $use_brew_sphinx
+    fish_add_path /usr/local/opt/sphinx-doc/bin
+end
