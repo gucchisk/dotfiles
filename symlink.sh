ec49b9dbe314e42f2770e6b87f66409b92c5cdbd
gucchisk <gucchi_sk@yahoo.co.jp>
2021-09-18 20:39:58 +0900

    format

--- symlink.sh
+++ symlink.sh
@@ -5,16 +5,16 @@ dotfilelist=(.emacs .emacs.d/elisp .emacs.d/conf .gitconfig .emacs.d/custom.el)
 dotfiledirlist=(.config/fish/functions .config/fish/conf.d)
 
 for dir in ${dotfiledirlist[@]}; do
-    files=$(ls $dir)
-    for file in $files; do
-	dotfilelist+=( $dir/$file )
-    done
+  files=$(ls $dir)
+  for file in $files; do
+    dotfilelist+=( $dir/$file )
+  done
 done
 
 for file in ${dotfilelist[@]}; do
-    src=$homedir/$file
-    dist=~/$file
-    if [ ! -L $dist ]; then
-	ln -s $src $dist
-    fi
+  src=$homedir/$file
+  dist=~/$file
+  if [ ! -L $dist ]; then
+    ln -s $src $dist
+  fi
 done
