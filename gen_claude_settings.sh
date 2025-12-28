3f54702dfaa4b2d879a88c5d636593696573d5f3
gucchisk <gucchi_sk@yahoo.co.jp>
2025-12-29 04:59:54 +0900

    revert

--- gen_claude_settings.sh
+++ gen_claude_settings.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
 if [ -f ~/.claude/settings.json.local ]; then
-
-jq -s '
-  def merge:
-    reduce .[] as $item ({}; 
-      reduce ($item | keys_unsorted[]) as $key (.;
-        if ($item[$key] | type) == "array" then
-          .[$key] = ((.[$key] // []) + $item[$key] | unique)
-        elif ($item[$key] | type) == "object" then
-          .[$key] = ([.[$key], $item[$key]] | merge)
-        else
-          .[$key] = $item[$key]
-        end
-      )
-    );
-  merge
-' .claude/settings.json.base ~/.claude/settings.json.local > ~/.claude/settings.json
+  jq -s '.[0] * .[1]' .claude/settings.json.base ~/.claude/settings.json.local > ~/.claude/settings.json
 else
   cp .claude/settings.json.base ~/.claude/setting.json
 fi
