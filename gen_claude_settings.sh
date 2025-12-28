5d4c5a1badfbb95ac2632b00fc59537dc45e0059
gucchisk <gucchi_sk@yahoo.co.jp>
2025-12-29 04:47:35 +0900

    chore: update gen_claude_settings

--- gen_claude_settings.sh
+++ gen_claude_settings.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 if [ -f ~/.claude/settings.json.local ]; then
-  jq -s '.[0] * .[1]' .claude/settings.json.base ~/.claude/settings.json.local > ~/.claude/settings.json
+  jq -s '
+reduce .[] as $item ({}; 
+  reduce ($item | keys[]) as $key (.;
+    if ($item[$key] | type) == "array" 
+    then .[$key] = ((.[$key] // []) + $item[$key])
+    else .[$key] = $item[$key]
+    end
+  )
+)' .claude/settings.json.base ~/.claude/settings.json.local > ~/.claude/settings.json
 else
   cp .claude/settings.json.base ~/.claude/setting.json
 fi
