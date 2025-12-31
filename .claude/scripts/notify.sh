025a9ef0f8a94203184f98dc2b79e279e197ec59
gucchisk <gucchi_sk@yahoo.co.jp>
2025-12-31 12:08:16 +0900

    chore: notify.sh

--- .claude/scripts/notify.sh
+++ .claude/scripts/notify.sh
@@ -4,5 +4,5 @@
 # osascript -e "display notification \"$MESSAGE\" with title \"Claude Code\""
 printf '\e]9;\a'
 printf '\e]1337;RequestAttention=1\a'
-osascript -e 'display alert "⚠️ Claude Code" message "入力が必要です" as critical buttons {"確認"} default button 1'
+# osascript -e 'display alert "⚠️ Claude Code" message "入力が必要です" as critical buttons {"確認"} default button 1'
 # osascript -e 'tell application "iTerm2" to activate' 2>/dev/null
